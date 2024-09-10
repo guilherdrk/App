4e6087c9f1c5fb6eea9d2c2d4b4f3d196d32a3f7
guilherdrk <guibem20@gmail.com>
2024-09-10 11:22:07 -0300

    Fundamentos 003

--- index.js
+++ index.js
@@ -2,20 +2,16 @@
 
 let meta = {
     value: 'Ler um livro todo mês',
-    address: 2,
     checked: false,
-    log: (info) => {
-        console.log(info);
-    }
+
 }
-meta.value = "não é mais ler um livro"
-meta.log(meta.value);
 
-//Function // Arrow function
-// const criarMeta = () => {
+let metas = [
+    meta,
+    {
+        value: "Treinar todos os dias",
+        checked: false 
+    }
+]
 
-// } 
-
-// function criarMeta() {
-
-// }
+console.log(metas[1].value);
