1e396c4baa2e6855fec990846f2eee03e2004be3
guilherdrk <guibem20@gmail.com>
2024-09-11 09:47:42 -0300

    Listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts');
+const { select, input, checkbox} = require('@inquirer/prompts');
 
 let meta = { 
     value: 'Tomar 3L de água por dia',
@@ -17,6 +17,38 @@ const cadastrarMeta = async ()=> {
         { value: meta, checked: false }
     );
 }
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas], // spread operator 
+        instructions: false
+    });
+
+    if(respostas.length == 0){
+        console.log("Nenhuma meta selecionada!");
+        return;
+    }
+
+    metas.forEach((m) => {
+        m.checked = false;
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta; // que retorna um valor logico (sim) ou (não)
+        }); //find serve para procurar
+
+        // if(meta.checked == false){
+        //     meta.checked = true;
+        // }else{
+        //     meta.checked = false;
+        // }
+        meta.checked = true;
+    }); // forEach(para cara)
+    console.log('Meta(s) marcadas como concluída(s)!');
+}
+
 const start = async () => {
  
     while(true){
@@ -45,12 +77,13 @@ const start = async () => {
                 console.log(metas);
                 break;
             case "listar":
-                console.log("vamos listar");
+                await listarMetas();
+                console.log(metas);
                 break;
             case "sair":
                 console.log("Até a proxima!");
                 return;    
-        }
+        };
     };
 };
 
